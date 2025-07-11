7dd297b1c85d649a6e04345b8e262026f098b259
snment <contactsnment@gmail.com>
2025-07-11 09:58:02 +0200

    good

--- txData/ESXLegacy_6BBD04.base/resources/r_bridge/config.lua
+++ txData/ESXLegacy_6BBD04.base/resources/r_bridge/config.lua
@@ -2,7 +2,7 @@
 --  _ __ | |__  _ __(_) __| | __ _  ___ 
 -- | '__|| '_ \| '__| |/ _` |/ _` |/ _ \
 -- | |   | |_) | |  | | (_| | (_| |  __/
--- |_|___|_.__/|_|  |_|\__,_|\__, |\___|
+-- |_|___|_.__|_|  |_|\__,_|\__, |\___|
 --  |_____|                  |___/      
 --
 --  Need support? Join our Discord server for help: https://discord.gg/rscripts
@@ -10,6 +10,9 @@
 Cfg = {
     -- Check if the resource is up to date. Recommended to keep it enabled.
     VersionCheck = true,
+
+    Framework = 'esx', -- 'esx', 'qb'
+    Inventory = 'ox',  -- 'ox', 'qb', 'esx', 'custom'
 
     -- Choose the notification system you want to use. 'custom' can be configured in your frameworks bridge.
     Notification = 'ox', -- 'default', 'ox', 'custom'
